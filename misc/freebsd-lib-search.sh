42b7509bdc07cf2a14af0b83b3f8f4b52f068f85
Amzo <Amzo@Archsbd.com>
2013-06-30 23:39:40 +0100

    Can check for libs in packages not installed now

--- misc/freebsd-lib-search.sh
+++ misc/freebsd-lib-search.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 #########################################################
 # Find all binary files in world that is linked against #
@@ -8,21 +8,49 @@
 
 
 match=$1
-
+match2=$2
 if [ -z $match ]; then
-  echo 'Usage: freebsd-lib-search <libname>'
+  echo 'Usage: freebsd-lib-search <libname> <Path to packages/'
   echo ''
   echo 'Find packages that depend on a given library.'
-  echo 'E.G freebsd-lib-search libarchive.so.3'
+  echo 'E.G freebsd-lib-search libarchive.so.3 ~/packages/'
   echo ''
   exit 1
 fi
 
-for file in \
-`find /bin /usr/bin /sbin /usr/sbin /lib /usr/lib/ /usr/local/lib -type f -print0 | xargs -0 file | grep ELF | awk  -F ":" '{print $1}'`; do
-      readelf -dW $file 2> /dev/null | sed -ne '/NEEDED/{s/^.*\[\(.*\)\]$/\1/;p;}' | grep $match > /dev/null 2>&1;
-      if [ $? -eq 0 ]; then 
-        echo $file depends on $match; 
-        pacman -Qo $file;
-      fi; 
-done 
+#Set some default variables
+cur_dir=`pwd`
+tmp_dir="${cur_dir}/tmp/"
+pac_path=$match2
+
+
+if [ ! -d "$tmp_dir" ]; then
+	mkdir "$tmp_dir"
+fi
+
+if [ -e "$cur_dir"/rebuild.txt ]; then
+	echo "" > "$cur_dir"/rebuild.txt
+fi
+
+
+find_libs() {
+
+
+for files in `find "$tmp_dir"/usr/{bin,sbin,lib} -type f -print0 | xargs -0 file | grep ELF | awk  -F ":" '{print $1}'`; do
+	readelf -dW $files 2> /dev/null | sed -ne '/NEEDED/{s/^.*\[\(.*\)\]$/\1/;p;}' | grep $match > /dev/null 2>&1 >/dev/null;
+	
+if [ $? -eq 0 ]; then
+        echo $packages Needs rebuild >> "$cur_dir"/rebuild.txt;
+	break
+fi
+
+done
+}
+
+for packages in `ls $pac_path/*.x?`; do
+	tar -xf "$packages" -C $tmp_dir
+
+	find_libs
+	#Clean tmp dir
+        rm -rf "$tmp_dir"/*
+done
