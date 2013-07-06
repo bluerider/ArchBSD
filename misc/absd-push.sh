e133c522bc5047bf08e45d1f6249d3b60d5ab306
Wolfgang Bumiller <wry.git@bumiller.com>
2013-07-06 16:07:40 +0200

    renamed update-pkg.sh -> absd-pkg.sh;; added absd-push.sh: sign and upload packages to the server into ~/Packages/<repo>/

---
+++ misc/absd-push.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+die() {
+	echo "*** $@"
+	exit 1
+}
+
+arg0=$0
+repo=$1
+shift
+
+case "$repo" in
+	core|extra|community|testing)
+		echo "Pushing to repository: $repo"
+		;;
+	*)
+		die "usage: $arg0 repository files..."
+		;;
+esac
+
+uplist=()
+for i in "$@"; do
+	[ -f "${i}.sig" ] || gpg --detach-sign "$i" || die "signing failed"
+	uplist=("${uplist[@]}" "./$i" "./${i}.sig")
+done
+
+scp "${uplist[@]}" "archbsd.net:Packages/$repo/"
