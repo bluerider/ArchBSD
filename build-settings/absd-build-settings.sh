6a0c35472cd30a4b88c617414941a955e17afd97
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2013-08-05 15:12:29 +0200

    More comments in absd-build-settings.sh...

--- build-settings/absd-build-settings.sh
+++ build-settings/absd-build-settings.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Put this file here: /etc/archbsd-build.conf
+# and modify it as needed
 
 #cachedir=/var/cache/pacman/pkg
 
 # DO NOT USE $HOME unless you want stuff to end up in /root
-abstree=/home/wry/Sources/ArchBSD
-buildtop=/home/wry/ABSD-Build
+# These 2 lines needs to be fixed and uncommented
+#abstree=/home/wry/Sources/ArchBSD
+#buildtop=/home/wry/ABSD-Build
 
+# These can be changed if necessary
 #package_output=${buildtop}/output
 #builder_bashrc=${buildtop}/bashrc
 #setup_script=${buildtop}/setup_root
